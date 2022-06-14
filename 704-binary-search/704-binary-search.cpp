0f946a65d5f61468700bc75a590b71da2b7fb811
Alok sinha <89570069+AlokPng@users.noreply.github.com>
2022-06-14 20:31:25 +0530

    Time: 107 ms (5.14%), Space: 27.6 MB (13.25%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-     int pivot, left = 0, right = nums.size() - 1;
+      int pivot, left = 0, right = nums.size() - 1;
     while (left <= right) {
       pivot = left + (right - left) / 2;
       if (nums[pivot] == target) return pivot;
