18ab161763b8f3dbba4feac58fc29f99a40fc8fc
Alok sinha <89570069+AlokPng@users.noreply.github.com>
2022-06-21 20:12:20 +0530

    Time: 21 ms (10.48%), Space: 7.1 MB (46.16%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, int> smap, tmap;
+
+    for (int i=0; i < s.size(); i++) {
+        if (smap.find(s[i]) == smap.end()) {
+            smap[s[i]] = i;
+        }
+        
+        if (tmap.find(t[i]) == tmap.end()) {
+            tmap[t[i]] = i;
+        }
+        
+        if (smap[s[i]] != tmap[t[i]])
+            return false;
+    }
+    
+    return true;
+
+        
+    }
+};
