2174acaae734be8a963a2e3c935fe333ba8552e1
Alok sinha <89570069+AlokPng@users.noreply.github.com>
2022-10-03 14:49:56 +0530

    Time: 22 ms (38.66%), Space: 20.1 MB (92.11%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -11,12 +11,16 @@
  */
 class Solution {
 public:
+    int res=0;
     int height(TreeNode* root)
     {
         if(root==NULL)
             return 0;
-        else
-            return max(height(root->left),height(root->right))+1;  
+        
+           int lh =height(root->left);
+        int rh =height(root->right);
+        res=max(res,(1+lh+rh));
+        return max(rh,lh)+1;
     }
     
  
@@ -26,8 +30,6 @@ public:
 
         if(root==NULL)
             return 0;
-       int d1=height(root->left)+height(root->right);
-        int d2=diameterOfBinaryTree(root->left);
-        int d3=diameterOfBinaryTree(root->right);
-        return max({d1,d2,d3});}
+     int data=height(root);
+        return res-1;}
 };
